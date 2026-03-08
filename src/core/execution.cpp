d3ad3041863638c1eb3a33a19a46a8e370c17e13
Svyatoslav Eylkin <elkinslava3@gmail.com>
2026-03-08 05:27:52 +0300

    refactored execution module

---
+++ src/core/execution.cpp
@@ -0,0 +1,28 @@
+#include "../../include/execution.h"
+#include <chrono>
+
+SimulatedExecutionHandler::SimulatedExecutionHandler(std::queue<std::unique_ptr<Event>>& events_) :
+	events(events_) {}
+
+void SimulatedExecutionHandler::execute_order(const Event& event) {
+	if (event.type() != EventType::ORDER) {
+		return;
+	}
+
+	const auto& order_event = static_cast<const OrderEvent&>(event);
+
+	// In a simulated environment, we assume the order is filled immediately.
+	// The fill_cost is set to 0.0 here because the NaivePortfolio
+	// calculates the fill cost based on the latest market price
+	// when it receives the FillEvent.
+
+	auto fill_time = std::chrono::system_clock::now();
+
+	auto fill_event = std::make_unique<FillEvent>(fill_time, order_event.get_symbol(), "SIM_EXCHANGE",
+												  order_event.get_quantity(), order_event.get_direction(),
+												  0.0, // fill_cost (to be estimated by Portfolio)
+												  -1.0 // commission
+	);
+
+	events.push(std::move(fill_event));
+}
